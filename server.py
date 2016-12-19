cdc09f4f1055bb09d519c0774890ea6326ee56ba
boubou19 <miisterunknown@gmail.com>
2016-12-19 23:49:56 +0100

    requesting ip function added
    added a function that handle a request for having an ip

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ Created on Mon Dec 19 23:12:28 2016
 """
 
 class ip:
-    def __init__(self,name,access_ip,access_port,used):
+    def __init__(self,name,access_ip,access_port,used=False):
         self.name = name
         self.acces_ip = access_ip
         self.access_port = access_port
@@ -22,8 +22,16 @@ liste_objet = []
 
 def construction_table():
     for i in range(0,len(liste_ip)):
-        liste_objet.append(ip(liste_nom[i],liste_ip[i],liste_port[i],0))
+        liste_objet.append(ip(liste_nom[i],liste_ip[i],liste_port[i]))
 
+def reception_requete(ip):
+    for i in range(0,len(liste_objet)):
+        if liste_objet.access_ip == ip:
+            if liste_objet.used:
+                raise NameError("ip déja occupee")
+            else:
+                liste_objet.used = True
+    return 0
     
 if __name__ == "__main__":
     liste_ip = [1,2,3,4,5]
