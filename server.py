99449605a7aee5b58c26c859ed433f66622c5cfb
boubou19 <miisterunknown@gmail.com>
2016-12-19 23:56:35 +0100

    fixed the request function
    now liste_objet is correctly typed

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ Created on Mon Dec 19 23:12:28 2016
 class ip:
     def __init__(self,name,access_ip,access_port,used=False):
         self.name = name
-        self.acces_ip = access_ip
+        self.access_ip = access_ip
         self.access_port = access_port
         self.used = used
         
@@ -26,11 +26,11 @@ def construction_table():
 
 def reception_requete(ip):
     for i in range(0,len(liste_objet)):
-        if liste_objet.access_ip == ip:
-            if liste_objet.used:
+        if liste_objet[i].access_ip == ip:
+            if liste_objet[i].used:
                 raise NameError("ip déja occupee")
             else:
-                liste_objet.used = True
+                liste_objet[i].used = True
     return 0
     
 if __name__ == "__main__":
@@ -40,3 +40,13 @@ if __name__ == "__main__":
 
     construction_table()
     print(liste_objet)
+    
+    #on setup vite fait l'ip un en mode utilisé
+    reception_requete(1)
+    #on retente la meme pour forcer une erreur
+    try:
+        reception_requete(1)
+    #il se passera rien dans la console parce qu'on a catchée l'exception ;)
+    except NameError:
+        pass
+    
