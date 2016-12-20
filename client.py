b8172a005ca739bb5d6aab0f3ec9287750d0b2ea
boubou19 <miisterunknown@gmail.com>
2016-12-20 20:57:09 +0100

    updated server.py and added client.py but not functional

---
+++ client.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 17:20:52 2016
+
+@author: alexis
+"""
+import socket
+
+### a faire
--- server.py
+++ server.py
@@ -4,7 +4,15 @@ Created on Mon Dec 19 23:12:28 2016
 
 @author: alexis
 """
+import socket
+import threading
 
+ips = "ip.txt"
+noms = "noms.txt"
+ports = "ports.txt"
+ip_serveur = "127.0.0.1"
+port_serveur = 9999
+n_co = 1
 class ip:
     def __init__(self,name,access_ip,access_port,used=False):
         self.name = name
@@ -19,10 +27,20 @@ liste_port = []
 liste_nom = []
 #reste vide
 liste_objet = []
+def import_data(fichier,liste):
+    with open(fichier,"r") as fichier:
+        for ligne in fichier:
+            liste.append(ligne.strip())
+
 
 def construction_table():
+    import_data(ips,liste_ip)
+    import_data(noms,liste_nom)
+    import_data(ports,liste_port)
+    
     for i in range(0,len(liste_ip)):
         liste_objet.append(ip(liste_nom[i],liste_ip[i],liste_port[i]))
+    return 0
 
 def reception_requete(ip):
     for i in range(0,len(liste_objet)):
@@ -33,20 +51,20 @@ def reception_requete(ip):
                 liste_objet[i].used = True
     return 0
     
-if __name__ == "__main__":
-    liste_ip = [1,2,3,4,5]
-    liste_port = [1,2,3,4,5]
-    liste_nom = [1,2,3,4,5]
+def envoi_proxy(ip):
+   
 
-    construction_table()
-    print(liste_objet)
+    return 0
+if __name__ == "__main__":
     
-    #on setup vite fait l'ip un en mode utilisé
-    reception_requete(1)
-    #on retente la meme pour forcer une erreur
+    serveur = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     try:
-        reception_requete(1)
-    #il se passera rien dans la console parce qu'on a catchée l'exception ;)
-    except NameError:
-        pass
+        serveur.bind(ip_serveur,port_serveur)
+    except socket.error:
+        print("impossible de lier le serveur a l'ip et au port choisi")
+    while True:
+        serveur.listen(n_co)
+        connexion,addresse = serveur.accept()
+        ### a completer
+    
     
