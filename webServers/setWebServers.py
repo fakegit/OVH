d0687d2506c9db13cb741f6c2a637a930f3fd4f1
nwmqpa <thomasnicollet@yahoo.fr>
2016-12-21 23:25:48 +0100

    Minor changement for visuals

--- webServers/setWebServers.py
+++ webServers/setWebServers.py
@@ -30,8 +30,10 @@ with open("commands","r") as r:
 
 if mode == "firefox":
     for i in commandList.split("\n"):
+        print("Sending :",i.format(vps,"Proxy"))
         os.system(i.format(vps,"Firefox"))
 elif mode == "proxy":
     commandList = commandList + "\n" + '"mv /root/setupProxy/proxy.txt ../webServer/cgi-scripts/proxy.txt"'
     for i in commandList.split("\n"):
+        print("Sending :",i.format(vps,"Proxy"))
         os.system(i.format(vps,"Proxy"))
