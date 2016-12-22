e7d2a71f7781111ecc5d8b9cc4b78ce55f898d73
nwmqpa <thomasnicollet@yahoo.fr>
2016-12-22 14:28:35 +0100

    Fixing stupid mistakes

--- webServers/setupServer.py
+++ webServers/setupServer.py
@@ -14,7 +14,7 @@ import os
 apachePath = "/etc/apache2/apache2.conf"
 apacheModif = "<Directory /var/www/html/cgi-scripts>\n	Allow from all\n	Options FollowSymLinks\n	Options +ExecCGI\n	AddHandler cgi-script .py\n</Directory>"
 hostname = subprocess.Popen(["hostname"],stdout=subprocess.PIPE).communicate()[0].decode().split('\n')[0]
-setupServerCommands = ["ssh root@{0} sudo service apache2 restart"]
+setupServerCommands = ["sudo service apache2 restart"]
 
 '''
 ###############################################################
@@ -28,4 +28,4 @@ with open(apachePath,"a") as a:
     a.write(apacheModif)
 
 for i in setupServerCommands:
-    os.system(i.format(hostname))
+    os.system(i)
