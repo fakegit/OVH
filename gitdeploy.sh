2ce4a13b7a75c6753bb53998efe055bcad5bc97d
nwmqpa <thomasnicollet@yahoo.fr>
2017-06-08 08:04:54 -0400

    Create gitdeploy.sh

--- gitdeploy.sh
+++ gitdeploy.sh
@@ -3,8 +3,8 @@ str=$( eval "git ls-remote --tags https://github.com/nwmqpa/OVH")
 if echo "$str" | grep 'PYTHON'; then
     exit
 else
-  git config --global user.email $BUILDEREMAIL
-  git config --global user.name $BUILDERNAME
+  git config --global user.email "$BUILDEREMAIL"
+  git config --global user.name "$BUILDERNAME"
   export GIT_TAG=v$TRAVIS_BUILD_NUMBER
   git tag $GIT_TAG -a -m "Build #$TRAVIS_BUILD_NUMBER"
   git push -q https://$TAGPERM@github.com/nwmqpa/OVH --tags
